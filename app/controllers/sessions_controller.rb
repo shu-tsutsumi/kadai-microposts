cdf793318cdee62336f85ada0cce91de66fabff0
Shuhei.T <aaa@bbb.ccc>
2018-05-02 05:31:25 +0000

    timeline

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
     end
   end
 
-  def destory
+  def destroy
     session[:user_id] = nil
     flash[:success] = 'ログアウトしました'
     redirect_to root_url
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,4 +3,9 @@ class Micropost < ApplicationRecord
   
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 255}
+  
+  
+  def all_microposts
+    Micropost.all
+  end
 end
